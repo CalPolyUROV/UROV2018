ebe0e907eec1ac193a9db64568eb2d1185637c2a
Spencer Shaw <sfshaw@calpoly.edu>
2018-03-16 14:46:05 -0700

    clean up camera cpp

--- Arduino/cameras.cpp
+++ Arduino/cameras.cpp
@@ -1,3 +1,6 @@
+int _c1 = 22;
+int _c2 = 24;
+int _c3 = 26;
 #include "arduino.h"
 #include <Wire.h>
 #include <Servo.h>
@@ -5,31 +8,28 @@
 #define NUM_CAMERAS 2
 
 //camera pins
-int _c1 = 22;
-int _c2 = 24;
-int _c3 = 26;
 int currentCamera = 0;
 bool debounce = 0;
 
 void setCameras(unsigned char buttons)
 {
-    
-    if(CHECK_BIT(buttons, 2)&&debounce){
-        debounce = 0;
-        //digitalWrite(13, HIGH);
-        currentCamera++;
-        if(currentCamera==NUM_CAMERAS) {currentCamera = 0;}
-        
-        digitalWrite(22,!(CHECK_BIT(currentCamera, 0)));
-        digitalWrite(24,CHECK_BIT(currentCamera, 1));
-        digitalWrite(26,CHECK_BIT(currentCamera, 2));
-       
+
+  if (CHECK_BIT(buttons, 2) && debounce) {
+    debounce = 0;
+    //digitalWrite(13, HIGH);
+    currentCamera++;
+    if (currentCamera == NUM_CAMERAS) {
+      currentCamera = 0;
     }
-    else {
-      debounce = 1;
-        //digitalWrite(13, LOW);
-    }
-    
-    
-    
+    digitalWrite(_c1, !(CHECK_BIT(currentCamera, 0)));
+    digitalWrite(_c2, CHECK_BIT(currentCamera, 1));
+    digitalWrite(_c3, CHECK_BIT(currentCamera, 2));
+  }
+  else {
+    debounce = 1;
+    //digitalWrite(13, LOW);
+  }
+
+
+
 }
