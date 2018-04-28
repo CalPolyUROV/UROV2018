427e3d7e0704d9049e77122b044f2edd8c8a5aa1
Spencer Shaw <sfshaw@calpoly.edu>
2018-04-28 01:44:50 -0700

    Modularize camera controls

--- Arduino/cameras.cpp
+++ Arduino/cameras.cpp
@@ -8,38 +8,53 @@
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
 //camera pins
-
 int currentCamera = 0;
 bool debounce = 0;
 
-void enableCameras(bool enable)
+
+void camera_setup()
+{
+
+  enable_cameras(CAMERA_ENABLE);
+}
+
+void enable_cameras(bool enable)
 {
   //camera multiplexor enable is active low
-  if (enable) {
-    digitalWrite(camera_enable, LOW);
+  if (enable)
+  {
+    digitalWrite(CAMERA_ENABLE_PIN, LOW);
   }
-  else {
-    digitalWrite(camera_enable, HIGH);
+  else
+  {
+    digitalWrite(CAMERA_ENABLE_PIN, HIGH);
   }
 }
 void setCameras(unsigned char buttons)
 {
 
-  if (CHECK_BIT(buttons, 2) && debounce) {
+
+  if (CHECK_BIT(buttons, 2) && debounce)
+  {
     debounce = 0;
     currentCamera++;
-    if (currentCamera == NUM_CAMERAS) {
+    if (currentCamera == NUM_CAMERAS)
+    {
       currentCamera = 0;
     }
-    digitalWrite(CAMERA_SEL_PIN_A, !(CHECK_BIT(currentCamera, 0)));
-    digitalWrite(CAMERA_SEL_PIN_B, CHECK_BIT(currentCamera, 1));
-    digitalWrite(CAMERA_SEL_PIN_C, CHECK_BIT(currentCamera, 2));
   }
   else
   {
     debounce = 1;
   }
 
-
-
+  write_cameras();
 }
+void write_cameras()
+{
+
+  digitalWrite(CAMERA_SEL_PIN_A, CHECK_BIT(currentCamera, 0));
+  digitalWrite(CAMERA_SEL_PIN_B, CHECK_BIT(currentCamera, 1));
+  digitalWrite(CAMERA_SEL_PIN_C, CHECK_BIT(currentCamera, 2));
+}
+
