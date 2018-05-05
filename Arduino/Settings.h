2fba7df8aa3a4664357b34638ea3cacba47e7cda
Spencer Shaw <sfshaw@calpoly.edu>
2018-05-05 01:22:35 -0700

    move motor pins

--- Arduino/Settings.h
+++ Arduino/Settings.h
@@ -19,10 +19,10 @@
 //Thrusters
 #define MOTOR_A_PIN 8
 #define MOTOR_B_PIN 9
-#define MOTOR_C_PIN 12
-#define MOTOR_D_PIN 13
-#define MOTOR_E_PIN 10
-#define MOTOR_F_PIN 11
+#define MOTOR_C_PIN 10
+#define MOTOR_D_PIN 11
+#define MOTOR_E_PIN 12
+#define MOTOR_F_PIN 13
 // Cameras
 #define CAMERA_SERVO_PIN 2
 #define CAMERA_ENABLE_PIN 44
